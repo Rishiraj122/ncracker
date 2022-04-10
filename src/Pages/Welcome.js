681cc692229017ac64ddd1c097bdade8e87f9f22
Rishiraj <52316252+Rishiraj122@users.noreply.github.com>
2022-04-10 13:52:35 +0530

    Update Welcome.js

--- src/Pages/Welcome.js
+++ src/Pages/Welcome.js
@@ -4,6 +4,7 @@ function Welcome(){
     return(
         <div>
             <h1>Site Hijacked</h1>
+            <img src="https://cdn.dribbble.com/users/1405795/screenshots/12129013/media/20996c6c6c6fed3e048cd1eb04548f51.png?compress=1&resize=400x300"/>
         </div>
     );
 }
