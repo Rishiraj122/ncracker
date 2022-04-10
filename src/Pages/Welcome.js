6642970b4712066d033b9e8cc92adc46d5161103
Rishiraj <52316252+Rishiraj122@users.noreply.github.com>
2022-04-10 13:49:31 +0530

    Added Image

--- src/Pages/Welcome.js
+++ src/Pages/Welcome.js
@@ -3,7 +3,8 @@ import React from "react";
 function Welcome(){
     return(
         <div>
-            <h1>Welcome To Chutkula Vines</h1>
+            <h1>Site Hijacked</h1>
+            <img src="https://cdn.dribbble.com/users/1405795/screenshots/12129013/media/20996c6c6c6fed3e048cd1eb04548f51.png?compress=1&resize=400x300">
         </div>
     );
 }
